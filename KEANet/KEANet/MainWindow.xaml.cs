34497c03ac9670cf95c1d25c8633439cc36136dd
Leiki á Mýrini <1065489@ucn.dk>
2019-10-22 13:51:50 +0200

    ui name and event handlers

--- KEANet/KEANet/MainWindow.xaml.cs
+++ KEANet/KEANet/MainWindow.xaml.cs
@@ -28,5 +28,30 @@ namespace KEANet
         private void listbox_left_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
         }
+
+        private void GreaterThanButton_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("greater than button");
+        }
+
+        private void LesserThanButton_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("lesser than button");
+        }
+
+        private void BuyButton_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("buy button");
+        }
+
+        private void checkbox_Checked(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("checkbox checked");
+        }
+
+        private void checkbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("checkbox unchecked");
+        }
     }
 }
