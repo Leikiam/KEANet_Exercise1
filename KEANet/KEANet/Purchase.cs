7e006ef80c4055d69abe83d6a589bdd6ebce46e8
MSI\Bastian <banoga@live.dk>
2019-10-22 13:00:41 +0200

    Purchase class with constructor missing methods

---
+++ KEANet/KEANet/Purchase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEANet
+{
+    class Purchase
+    {
+        private Boolean InternetConnection;
+        private int PhoneLines;
+        private string[] CellPhone;
+        private int Price;
+
+        public Purchase()
+        {
+            Boolean internet = InternetConnection;
+            int phoneLines = PhoneLines;
+            string[] cellPhone = CellPhone;
+            int price = Price;
+        }
+
+
+        
+}
+        
+       
+        
+    }
+
